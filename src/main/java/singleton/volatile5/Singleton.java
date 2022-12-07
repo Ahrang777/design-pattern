eea58de37cd18a7ccb1ed55c134aafbbc313ffee
JunginPark4364 <wjddlsee@gmail.com>
2022-12-07 17:19:36 +0900

    feat: 싱글톤 구현(volatile 사용)

---
+++ src/main/java/singleton/volatile5/Singleton.java
@@ -0,0 +1,25 @@
+package singleton.volatile5;
+
+/**
+ * volatile을 이용한 명령어 reorder 금지
+ */
+public class Singleton {
+    private volatile static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) { // 1번 체크
+            synchronized (Singleton.class) {
+                if (instance == null) { // 2번 체크 >> double check
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    // 관련 메서드 ...
+}
