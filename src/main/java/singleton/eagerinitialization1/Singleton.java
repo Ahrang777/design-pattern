07838b4d0017742047d294909a7de7c9f60d8f6c
JunginPark4364 <wjddlsee@gmail.com>
2022-12-07 17:15:30 +0900

    feat: 싱글톤 구현(Eager Initialization)

---
+++ src/main/java/singleton/eagerinitialization1/Singleton.java
@@ -0,0 +1,21 @@
+package singleton.eagerinitialization1;
+
+/**
+ * Eager Initialization
+ */
+public class Singleton {
+    private static Singleton instance = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    // 관련 메서드 ...
+    public void print() {
+        System.out.println("singleton pattern: 싱글 쓰레드 Eager Initialization");
+    }
+}
