88d5f7941197bf0a89b3f1ddcd8c66dac9cc7555
JunginPark4364 <wjddlsee@gmail.com>
2022-12-11 17:38:21 +0900

    build: 롬복 제거

--- src/main/java/creational/builder/lombokbuilder4/Book.java
+++ src/main/java/creational/builder/lombokbuilder4/Book.java
@@ -1,17 +1,14 @@
 package creational.builder.lombokbuilder4;
 
-import lombok.Builder;
-import lombok.NonNull;
-
 /**
  * lombok을 이용한 Builder 패턴
  */
-@Builder
+//@Builder
 public class Book {
-    @NonNull // 필수 속성임을 의미
+//    @NonNull // 필수 속성임을 의미
     private Long id;		// 필수
 
-    @NonNull
+//    @NonNull
     private String isbn;	// 필수
     private String title;	// 선택
     private String author;	// 선택
