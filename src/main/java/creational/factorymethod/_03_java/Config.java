482f56ff08e495b7e456dbea70ef387ab03d9211
JunginPark4364 <wjddlsee@gmail.com>
2022-12-12 15:27:57 +0900

    feat: 자바에서 팩토리 메서드 패턴이 적용된 예시

---
+++ src/main/java/creational/factorymethod/_03_java/Config.java
@@ -0,0 +1,11 @@
+package creational.factorymethod._03_java;
+
+//@Configuration
+public class Config {
+
+//    @Bean
+    public String hello() {
+        return "hello";
+    }
+
+}
---
+++ src/main/java/creational/factorymethod/_03_java/SpringBeanFactoryExample.java
@@ -0,0 +1,13 @@
+package creational.factorymethod._03_java;
+
+public class SpringBeanFactoryExample {
+    public static void main(String[] args) {
+//        BeanFactory xmlFactory = new ClassPathXmlApplicationContext("config.xml");
+//        String hello = xmlFactory.getBean("hello", String.class);
+//        System.out.println(hello);
+//
+//        BeanFactory javaFactory = new AnnotationConfigApplicationContext(Config.class);
+//        String hi = javaFactory.getBean("hello", String.class);
+//        System.out.println(hi);
+    }
+}
